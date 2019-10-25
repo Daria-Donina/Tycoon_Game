d7c8abd581e9c16bbd5a04159fc5f1f532340ba3
NadiaSob <sobo_na@mail.ru>
2019-10-25 15:51:06 +0300

    Changes tilemap to rectangle, added new grass, made animations for lion, created Animal script

---
+++ Tycoon_Game/Assets/Scripts/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveRight();
+    }
+
+    public void MoveRight()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
