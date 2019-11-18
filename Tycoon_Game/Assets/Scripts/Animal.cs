c60d6f9af79b38d5fcc2c2660c90794e84ae1393
NadiaSob <sobo_na@mail.ru>
2019-11-18 23:23:08 +0300

    Worked on Animal script, made the basis for making the animal move in random directions

--- Tycoon_Game/Assets/Scripts/Animal.cs
+++ Tycoon_Game/Assets/Scripts/Animal.cs
@@ -7,22 +7,70 @@ public class Animal : MonoBehaviour
     [SerializeField]
     private float speed;
 
-    private Animator animator;
+    private Vector3 direction;
+
+    private float waitTime;
+
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = new Vector3(0.0f, 1.0f, 0.0f);
+        waitTime = 2.0f;
+        timer =  0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveRight();
+        transform.position += direction * speed * Time.deltaTime;
+
+        timer += Time.deltaTime;
+
+        // Check if we have reached beyond 2 seconds.
+        // Subtracting two is more accurate over time than resetting to zero.
+        if (timer > waitTime)
+        {
+
+            // Remove the recorded 2 seconds.
+            timer -= waitTime;
+
+            //Меняем waitTime на случайное число в каком-то диапазоне
+
+            GoDown(); //Тут надо сделать так, чтобы вызывался случайных метод из GoUp(), GoDown(), GoLeft(), GoRight()
+        }
     }
 
-    public void MoveRight()
+    void GoUp()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        //Меняем направление
+        direction = new Vector3(0.0f, 1.0f, 0.0f);
+
+        //Переключаем анимацию
+    }
+
+    void GoDown()
+    {
+        //Меняем направление
+        direction = new Vector3(0.0f, -1.0f, 0.0f);
+
+        //Переключаем анимацию
+    }
+
+    void GoLeft()
+    {
+        //Меняем направление
+        direction = new Vector3(-1.0f, 0.0f, 0.0f);
+
+        //Переключаем анимацию
+    }
+
+    void GoRight()
+    {
+        //Меняем направление
+        direction = new Vector3(1.0f, 1.0f, 0.0f);
+
+        //Переключаем анимацию
     }
 }
