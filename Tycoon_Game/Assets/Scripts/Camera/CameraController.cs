24eee0b0f3dc3dea33a81bc335f10e1f7f3282a8
NadiaSob <sobo_na@mail.ru>
2019-12-13 01:37:29 +0300

    Now player can move the camera!

---
+++ Tycoon_Game/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector2 startPosition;
+
+    private new Camera camera;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float positionX = camera.ScreenToWorldPoint(Input.mousePosition).x - startPosition.x;
+            float positionY = camera.ScreenToWorldPoint(Input.mousePosition).y - startPosition.y;
+            transform.position = new Vector3(transform.position.x - positionX, 
+                transform.position.y - positionY, transform.position.z);
+        }
+    }
+}
