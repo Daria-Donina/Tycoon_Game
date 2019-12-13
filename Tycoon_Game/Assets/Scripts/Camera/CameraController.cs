0725e83ee6829598aa47e003354f885173524a3e
NadiaSob <sobo_na@mail.ru>
2019-12-13 21:31:18 +0300

    Now camera moves better, fixed lioness animation for walking up

--- Tycoon_Game/Assets/Scripts/Camera/CameraController.cs
+++ Tycoon_Game/Assets/Scripts/Camera/CameraController.cs
@@ -2,13 +2,22 @@
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+
     private Vector2 startPosition;
 
     private new Camera camera;
 
+    private float targetPositionX;
+
+    private float targetPositionY;
+
     private void Start()
     {
         camera = GetComponent<Camera>();
+        targetPositionX = transform.position.x;
+        targetPositionX = transform.position.y;
     }
 
     private void Update()
@@ -21,8 +30,15 @@ public class CameraController : MonoBehaviour
         {
             float positionX = camera.ScreenToWorldPoint(Input.mousePosition).x - startPosition.x;
             float positionY = camera.ScreenToWorldPoint(Input.mousePosition).y - startPosition.y;
-            transform.position = new Vector3(transform.position.x - positionX, 
-                transform.position.y - positionY, transform.position.z);
+            //transform.position = new Vector3(Mathf.Clamp(transform.position.x - positionX, -7.41f, 6.41f),
+            //    Mathf.Clamp(transform.position.y - positionY, -10.65f, 7.65f), transform.position.z);
+            targetPositionX = Mathf.Clamp(transform.position.x - positionX, -7.41f, 6.41f);
+            targetPositionY = Mathf.Clamp(transform.position.y - positionY, -10.65f, 7.65f);
+
         }
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, targetPositionX, speed * Time.deltaTime),
+            Mathf.Lerp(transform.position.y, targetPositionY, speed * Time.deltaTime),
+            transform.position.z);
     }
 }
